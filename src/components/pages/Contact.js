4d8c94bb323526027fe574ac46e2bb9531ecf612
Faz131 <fazms@icloud.com>
2023-05-20 16:37:30 -0700

    added vertical timeline

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,9 +1,103 @@
-import React from "react";
+// import React from "react";
+
+// function Contact() {
+//     return (
+//         <h1>Conatct Me</h1>
+//     )
+// };
+
+// export default Contact;
+
+
+import React, { useState } from "react";
+import axios from "axios";
+import '../../styles/Contact.css'
 
 function Contact() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [submitted, setSubmitted] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            // Send the form data to the server
+            const response = await axios.post("/send-email", {
+                name,
+                email,
+                message,
+            });
+
+            if (response.status === 200) {
+                setSubmitted(true);
+            } else {
+                console.error("Failed to send email.");
+            }
+        } catch (error) {
+            console.error("Failed to send email.", error);
+        }
+    };
+
+    if (submitted) {
+        return (
+            <div>
+                <h1>Thank you for your message!</h1>
+                <p>I'll get back to you as soon as possible.</p>
+                <p>You can also reach me on LinkedIn.</p>
+            </div>
+        );
+    }
+
     return (
-        <h1>Conatct Me</h1>
-    )
-};
+        <div className="contact-div">
+            <h1>Contact Me</h1>
+            <form onSubmit={handleSubmit} className="form-container">
+                <div className="form-element">
+                    <label htmlFor="name">Name:</label>
+                    <input
+                        type="text"
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="form-element">
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="form-element">
+                    <label htmlFor="message">Message:</label>
+                    <textarea
+                        id="message"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        required
+                    ></textarea>
+                </div>
+                <button type="submit" className="submit-button">Send Email</button>
+            </form>
+            <p>
+                You can also reach me on{" "}
+                <a
+                    href="https://www.linkedin.com/in/mike-fazio/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    LinkedIn
+                </a>
+                .
+            </p>
+        </div>
+    );
+}
 
 export default Contact;
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,11 +1,71 @@
 import React from "react";
+import { VerticalTimeline, VerticalTimelineElement }
+    from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import SchoolIcon from '@mui/icons-material/School'
+import WorkIcon from '@mui/icons-material/Work'
+import '../../styles/Resume.css'
 
 function Resume() {
     return (
 
-        <div>
-            <h1>My Resume</h1>
-            <h2>Available upon request</h2>
+        <div className="experience">
+            <VerticalTimeline lineColor="#B0A2C7">
+                <VerticalTimelineElement className="vertical-timeline-element--education"
+                    date="2007-2015"
+                    iconStyle={{ background: "#333", color: "#fff" }}
+                    icon={<SchoolIcon />} >
+                    <h3 className="vertical-timeline-element-title">University of Arizona
+                        Tucson, Arizona
+                    </h3>
+                    <h4>Bachelors of Applied Science</h4>
+                    <p>Network Administration</p>
+                </VerticalTimelineElement>
+
+                <VerticalTimelineElement className="vertical-timeline-element--education"
+                    date="Graduated June 2023"
+                    iconStyle={{ background: "#333", color: "#fff" }}
+                    icon={<SchoolIcon />} >
+                    <h3 className="vertical-timeline-element-title">University of Arizona
+                        Tucson, Arizona
+                    </h3>
+                    <h4>Full Stack Developer Bootcamp</h4>
+                    <p>Certification</p>
+
+                </VerticalTimelineElement>
+
+                <VerticalTimelineElement className="vertical-timeline-element--work"
+                    date="2004-2015"
+                    iconStyle={{ background: "#333", color: "#0a0652" }}
+                    icon={<WorkIcon />} >
+                    <h3 className="vertical-timeline-element-title">Teletech
+                    </h3>
+                    <h4>Customer Support/Lost Package Investigation</h4>
+                    <p>Assisted in front line customer support and promoted to the missing package department</p>
+                </VerticalTimelineElement>
+
+                <VerticalTimelineElement className="vertical-timeline-element--work"
+                    date="2015-2019"
+                    iconStyle={{ background: "#333", color: "#0a0652" }}
+                    icon={<WorkIcon />} >
+                    <h3 className="vertical-timeline-element-title">Callpointe
+                    </h3>
+                    <h4>Tech Support and Strategic Account Specialist</h4>
+                    <p>Handled inbound tech support calls promoted to the Strategic Accounts Team.  Worked with mySQL to update customer tables</p>
+                </VerticalTimelineElement>
+
+                <VerticalTimelineElement className="vertical-timeline-element--work"
+                    date="2019-Present"
+                    iconStyle={{ background: "#333", color: "#0a0652" }}
+                    icon={<WorkIcon />} >
+                    <h3 className="vertical-timeline-element-title">Integrated Axis Technology Group
+                    </h3>
+                    <h4>Sytems and Network Support</h4>
+                    <p>Handled trouble tickets pertaining to Windows and Network issues.</p>
+                </VerticalTimelineElement>
+
+            </VerticalTimeline>
+
 
         </div>
     )
