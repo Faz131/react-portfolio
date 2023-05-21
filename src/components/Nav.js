9777caabee9d88d25ad22043d9e638145850c11f
Faz131 <fazms@icloud.com>
2023-05-20 18:52:09 -0700

    fixed hamburger button

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,7 +7,7 @@ function Nav({ currentPage, handlePageChange }) {
 
         <nav className="navbar navbar-expand-lg bg-dark">
             <div className="container-fluid">
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler bg-secondary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
